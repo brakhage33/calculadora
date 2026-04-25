ca7ea8b8d13da715b8a3c63aba22338ec7bf6f05
Mario <marioacunaadamuz@gmail.com>
2026-04-25 22:09:08 +0000

    Añadir túnel ssh

--- servidor/Servidor.java
+++ servidor/Servidor.java
@@ -5,11 +5,9 @@ public class Servidor {
  public static void main(String[] args) {
    try{
       // Crear el objeto que implementa los servicios
-      String ipRegistro = "registro-rmi.duckdns.org";
-      String URL = "rmi://" + ipRegistro + ":1099/CalculadoraServer";
       CalculadoraImpl obj=new CalculadoraImpl();
       // Registrarlo
-      Naming.rebind(URL,obj);
+      Naming.rebind("//localhost:1099/CalculadoraServer",obj);
       System.out.println("CalculadoraServer registrada");
    }
    catch (Exception e)
